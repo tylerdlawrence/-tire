7e7f55efdad224b335eecfe6602eab11e70c224d
Tyler Lawrence <tyler.lawrence@hey.com>
2023-03-03 23:55:53 -0500

    Initial Commit

---
+++ @tire/_tireApp.swift
@@ -0,0 +1,17 @@
+//
+//  _tireApp.swift
+//  @tire
+//
+//  Created by Tyler D Lawrence on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct _tireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
